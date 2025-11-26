885d6a934f489454c4549f4d5cccfa2989374280
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2025-11-25 22:22:08 -0800

    added opengraph data

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,22 @@ const dmSans = DM_Sans({
 export const metadata = {
   title: "Olivia Oomen",
   description: "Olivia Oomen's Industrial Design Portfolio",
+  openGraph: {
+    title: "Olivia Oomen / Portfolio",
+    description: "I'm an interdisciplinary creative focused on using design as a tool to shift culture and improve lives",
+    url: "https://oliviaoomen.com",
+    siteName: "Olivia Oomen / Portfolio",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Social preview image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
