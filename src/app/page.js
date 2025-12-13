e65a568ec6d46856cb2185f8cc623b14701b5854
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2025-12-13 09:18:52 -0800

    right projects remain

--- src/app/page.js
+++ src/app/page.js
@@ -4,21 +4,15 @@ import { unstable_noStore as noStore } from "next/cache";
 import HomeClient from '@/components/homepage/HomeClient';
 
 export default async function Home() {
-
-  // Detect draft mode
   const { isEnabled: preview } = draftMode();
-
-  // Disable all caching in preview mode
+  
   if (preview) {
     noStore();
   }
 
   const work = await fetchEntries('workListingPage', preview);
 
-  // console.log('Fetched Projects:', projects);
-
-  const projectData = work[0].fields.projectPages
-    .filter(project => project.fields.displayed)
+  const allProjects = work[0].fields.projectPages
     .map(project => ({
       title: project.fields.externalTitle || 'Project Title',
       slug: project.fields.slug,
@@ -26,9 +20,10 @@ export default async function Home() {
       imageWidth: project.fields.iconPicture.fields.image.fields.file.details.image.width,
       imageHeight: project.fields.iconPicture.fields.image.fields.file.details.image.height,
       homepageIconResizeFactor: project.fields.homepageIconResizeFactor || 1,
+      displayed: project.fields.displayed,
     }));
 
-  // console.log('Project Data:', projects[0].fields.displayed);
+  const displayedProjects = allProjects.filter(project => project.displayed);
 
-  return <HomeClient projectData={projectData} />;
+  return <HomeClient allProjects={allProjects} displayedProjects={displayedProjects} />;
 }
--- src/components/homepage/HomeClient.js
+++ src/components/homepage/HomeClient.js
@@ -1,30 +1,27 @@
 'use client'
-
 import { useEffect, useState } from 'react';
 import PhysicsWrapper from '@/components/homepage/PhysicsWrapper';
 import ProjectListingPage from '@/components/homepage/ProjectListingPage';
 
-export default function HomeClient({ projectData }) {
-  const [isBigScreen, setIsBigScreen] = useState(null); // null: unknown screen size
+export default function HomeClient({ allProjects, displayedProjects }) {
+  const [isBigScreen, setIsBigScreen] = useState(null);
 
   useEffect(() => {
     const checkScreen = () => {
       setIsBigScreen(window.innerWidth > 768);
     };
-
-    checkScreen(); // Set initial value
-
+    checkScreen();
     window.addEventListener('resize', checkScreen);
     return () => window.removeEventListener('resize', checkScreen);
   }, []);
 
-  if (isBigScreen === null) return null; // Or a loading fallback
+  if (isBigScreen === null) return null;
 
   return (
     <div className='container'>
       {isBigScreen
-        ? <PhysicsWrapper projectData={projectData} />
-        : <ProjectListingPage projectData={projectData} />}
+        ? <PhysicsWrapper projectData={displayedProjects} />
+        : <ProjectListingPage projectData={allProjects} />}
     </div>
   );
 }
